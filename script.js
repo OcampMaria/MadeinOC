d2fbbc1cefdfa5f6f0176417ee352244176ff67b
Ocampo <mariaocampo0513@gmail.com>
2022-04-03 18:40:38 -0700

    updated navbar with Grid

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
+// Mobile Menu Toggle Button JavaScript
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("mobile-menu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
-
-
-
-  
+}
