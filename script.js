685f928b6c4f625be121cc00e5c0e05d2997f63a
Ocampo <mariaocampo0513@gmail.com>
2022-04-24 17:29:06 -0700

    replaced navbar and added links to buttons and nav items on footer.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // Mobile Menu Toggle Button JavaScript
+// function myFunction() {
+//   var x = document.getElementById("mobile-menu");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
+
 function myFunction() {
-  var x = document.getElementById("mobile-menu");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    x.style.display = "none";
+    x.className = "topnav";
   }
 }
